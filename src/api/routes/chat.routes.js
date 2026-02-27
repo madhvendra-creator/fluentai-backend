2dbdbcce7966081b9a106cc55b366feec00ee081
MADHVENDRA TOMAR <madhvendratomar@MADHVENDRAs-MacBook-Air.local>
2026-02-27 18:03:57 +0530

    Remove JWT security for prototype testing

--- src/api/routes/chat.routes.js
+++ src/api/routes/chat.routes.js
@@ -1,23 +1,7 @@
 import { chatController } from "../controllers/chat.controller.js";
 
 export default async function chatRoutes(fastify, options) {
-    fastify.post("/chat/reply", {
-        preValidation: async (request, reply) => {
-            try {
-                await request.jwtVerify()
-            } catch (err) {
-                return reply.send(err)
-            }
-        }
-    }, chatController.handleReply);
+    fastify.post("/chat/reply", chatController.handleReply);
 
-    fastify.post("/chat/evaluate", {
-        preValidation: async (request, reply) => {
-            try {
-                await request.jwtVerify()
-            } catch (err) {
-                return reply.send(err)
-            }
-        }
-    }, chatController.handleEvaluate);
+    fastify.post("/chat/evaluate", chatController.handleEvaluate);
 }
