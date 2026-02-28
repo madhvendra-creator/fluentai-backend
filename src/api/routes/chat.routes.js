b8fe13d58783226591cb7642ea3eebbb2e535360
MADHVENDRA TOMAR <madhvendratomar@MADHVENDRAs-MacBook-Air.local>
2026-03-01 02:40:23 +0530

    Restore JWT security

--- src/api/routes/chat.routes.js
+++ src/api/routes/chat.routes.js
@@ -1,7 +1,7 @@
 import { chatController } from "../controllers/chat.controller.js";
 
 export default async function chatRoutes(fastify, options) {
-    fastify.post("/chat/reply", chatController.handleReply);
+    fastify.post("/chat/reply", { preValidation: [fastify.authenticate] }, chatController.handleReply);
 
-    fastify.post("/chat/evaluate", chatController.handleEvaluate);
+    fastify.post("/chat/evaluate", { preValidation: [fastify.authenticate] }, chatController.handleEvaluate);
 }
